bc0a1e967f61b32c4eda1ccecb6916f12af6cecd
Mitura <lumt@MobicaPL.local>
2019-01-07 15:53:12 +0100

    Fooling around with Vue event handling

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,36 @@
 import Vue from "vue";
 
-const app = new Vue({
-    el: "#app",
-    data: {
-        message: "Hello Vue!"
+Vue.component(
+    "todo-item",
+    {
+        props: ["title"],
+        template: "\
+            <li>\
+                {{ title }}\
+                <button v-on:click=\"$emit('remove')\">Remove</button>\
+            </li>\
+        "
     }
-});
+);
+
+new Vue({
+    el: "#todo-list-example",
+    data: {
+        newTodoText: "",
+        todos: [
+            { id: 1, title: "Do the dishes" },
+            { id: 2, title: "Take out the trash" },
+            { id: 3, title: "Walk the dog" }
+        ],
+        nextTodoId: 4
+    },
+    methods: {
+        addNewTodo: function() {
+            this.todos.push({
+                id: this.nextTodoId++,
+                title: this.newTodoText
+            });
+            this.newTodoText = "";
+        }
+    }
+})
