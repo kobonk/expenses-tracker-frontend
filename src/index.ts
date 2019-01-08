06fce230709e69ffc9c853f2f8017eb8f6b887cd
Mitura <lumt@MobicaPL.local>
2019-01-08 13:51:36 +0100

    First approach to loading and filtering categories list

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import Vue from "vue";
 import { backendUrl, language } from "config";
+
 const axios = require("axios");
-const translation = require(`./resources/i18n/${ language }.json`);
+const i18n = require(`./resources/i18n/${ language }.json`);
+const _ = require("lodash");
 
 type ExpenseCategory = {
     id:string;
@@ -18,22 +20,55 @@ type ExpenseFormTranslation = {
 }
 
 type ExpenseFormData = {
-    i18n:ExpenseFormTranslation
-    categories:Array<ExpenseCategory>
+    category:ExpenseCategory;
+    categoryName:string;
+    i18n:ExpenseFormTranslation;
+    matchingCategories:Array<ExpenseCategory>;
 }
 
+const blankCategory:ExpenseCategory = { id: "blank", name: "blank" };
+
 const data:ExpenseFormData = {
-    i18n: translation.addExpenseForm,
-    categories: []
+    category: blankCategory,
+    categoryName: blankCategory.name,
+    i18n: i18n.addExpenseForm,
+    matchingCategories: []
+};
+
+const autoCompleteField = Vue.component("auto-complete-field", {
+    props: ["value"],
+    template: '<input type="text" :value="value" @input="$emit(\'value-changed\', $event.target.value)">'
+});
+
+const findCategories = function(name:string):Promise<Array<ExpenseCategory>> {
+    return axios.get(`${ backendUrl }/categories`)
+    .then(function(response:AxiosResponse<Object>):Array<ExpenseCategory> {
+        return _.filter(response.data, (category:ExpenseCategory) => {
+            return _.includes(_.toLower(category.name), _.toLower(_.trim(name)));
+        });
+    })
+    .catch(function(error:Error):Array<any> {
+        console.log(error);
+
+        return [];
+    });
 };
 
 const vm = new Vue({
-    el: "#add-expense-form",
+    components: {
+        autoCompleteField: autoCompleteField
+    },
+    created: function() {
+        this.debouncedFindCategories = _.debounce(findCategories, 500);
+    },
     data: data,
-    methods: {}
-});
-
-axios.get(`${ backendUrl }/categories`)
-.then(function(response:AxiosResponse<Object>) {
-    data.categories = response.data;
+    el: "#add-expense-form",
+    watch: {
+        categoryName: function(newName:string, oldName:string) {
+            findCategories(newName)
+            .then(function(categories:Array<ExpenseCategory>) {
+                vm.matchingCategories = categories;
+            })
+        }
+    }
 });
