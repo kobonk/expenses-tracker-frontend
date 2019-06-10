c5915da7e0bef35b327ce442ef2fcc81221a87f9
Łukasz Mitura <kobonk@kobonk.pl>
2019-06-10 10:46:02 +0200

    Sorted filtered Expenses and displayed the average

--- src/components/filtered-expenses/index.ts
+++ src/components/filtered-expenses/index.ts
@@ -1,7 +1,9 @@
 import Expense from "types/Expense";
 import { ExpensesTableData } from "./../../ExpensesTable";
 import i18n from "./../../utils/i18n";
+import { extractMonthName, formatNumber } from "./../../utils/stringUtils";
 import { component as dataTableComponent } from "./../data-table/data-table";
+import AverageMonthly from './../average-monthly';
 import ViewTitle from './../view-title';
 
 const _ = require("lodash");
@@ -9,22 +11,50 @@ const _ = require("lodash");
 const component = {
     components: {
         "data-table": dataTableComponent,
+        "average": AverageMonthly,
         "view-title": ViewTitle
     },
     computed: {
-        expenses() {
-            return !this.expensesMap ? {} : this.expensesMap;
+        total() {
+            if (!this.expensesMap) {
+                return 0;
+            }
+
+            return Object.keys(this.expensesMap)
+                .reduce(
+                    (total : number, month : string) => {
+                        return this.expensesMap[month]
+                            .reduce((monthTotal : number, expense : Expense) => {
+                                return monthTotal += expense.getCost();
+                            },
+                            total
+                        );
+                    },
+                    0
+                );
+        },
+        average() {
+            if (!this.expensesMap || !this.total) {
+                return 0;
+            }
+
+            return this.total / Object.keys(this.expensesMap).length;
         }
     },
     data() {
         return {
+            i18n: i18n,
             title: _.replace(i18n.filterExpensesForm.resultTitle, "{FILTER_TEXT}", this.filterText)
         };
     },
     methods: {
         createExpensesTableData(expenses: Array<Expense>): ExpensesTableData {
             return new ExpensesTableData(expenses);
-        }
+        },
+        displayMonth(month: string): string {
+            return extractMonthName(month);
+        },
+        formatNumber: formatNumber
     },
     props: ["expensesMap", "filterText", "onClose", "onEdit"],
     template: `
@@ -32,8 +62,9 @@ const component = {
             <view-title :on-close="onClose">
                 {{ title }}
             </view-title>
-            <div v-for="month in Object.keys(expensesMap)" :key="month">
-                <h2>{{ month }} <small>({{ expensesMap[month].length }})</small></h2>
+            <average :total="total" :number-of-months="Object.keys(expensesMap).length" />
+            <div v-for="month in Object.keys(expensesMap).sort().reverse()" :key="month">
+                <h2>{{ displayMonth(month) }} <small>({{ expensesMap[month].length }})</small></h2>
                 <data-table
                     class="columns-3"
                     :data="createExpensesTableData(expensesMap[month])"
--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -3,4 +3,19 @@ import * as data from "config.json";
 const language = (data as any).language;
 const i18n = require(`./../resources/i18n/${ language }.json`);
 
-export default i18n
+const translateNumericValue = (text : string, amount : number) => {
+    const reg = new RegExp("^{one}([^{]*){few}([^{]*){else}(.*$)", "gmi");
+    const patterns = reg.exec(text).slice(1);
+
+    if (amount === 1) {
+        return `${ amount } ${ patterns[0] }`;
+    }
+
+    if (amount === 2 || amount === 3) {
+        return `${ amount } ${ patterns[1] }`;
+    }
+
+    return `${ amount } ${ patterns[2] }`;
+}
+
+export default Object.assign(translateNumericValue, i18n);
