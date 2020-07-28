2801756025c9f215415222687912159458a812db
kobonk <kobonk@kobonk.pl>
2020-07-28 11:57:31 +0200

    Refactored AutoCompleteField behavior

--- src/components/AutoCompleteField.ts
+++ src/components/AutoCompleteField.ts
@@ -1,28 +1,44 @@
-import "./AutoCompleteFieldStyles.sass"
+import "./AutoCompleteFieldStyles.sass";
 import { debounce } from "utils/commonUtils";
 
-
 const getNextItem: Function = (items: string[], item: string): string => {
-    return item === items[items.length - 1] ? items[0] : items[items.indexOf(item) + 1];
+    return item === items[items.length - 1]
+        ? items[0]
+        : items[items.indexOf(item) + 1];
 };
 
 const getPreviousItem: Function = (items: string[], item: string): string => {
-    return item === items[0] ? items[items.length - 1] : items[items.indexOf(item) - 1];
+    return item === items[0]
+        ? items[items.length - 1]
+        : items[items.indexOf(item) - 1];
 };
 
-const getMatchingItems: Function = (items: string[], item: string): string[] => {
+const getMatchingItems: Function = (
+    items: string[],
+    item: string
+): string[] => {
     const trimmedItem = item.trim();
 
     return items
-        .filter((listItem: string): boolean => {
-            return (new RegExp(trimmedItem, "i")).test(listItem.trim());
-        })
-        .sort((itemA: string, itemB: string): number => {
-            const indexA: number = itemA.trim().toLowerCase().indexOf(trimmedItem.toLowerCase());
-            const indexB: number = itemB.trim().toLowerCase().indexOf(trimmedItem.toLowerCase());
+        .filter(
+            (listItem: string): boolean => {
+                return new RegExp(trimmedItem, "i").test(listItem.trim());
+            }
+        )
+        .sort(
+            (itemA: string, itemB: string): number => {
+                const indexA: number = itemA
+                    .trim()
+                    .toLowerCase()
+                    .indexOf(trimmedItem.toLowerCase());
+                const indexB: number = itemB
+                    .trim()
+                    .toLowerCase()
+                    .indexOf(trimmedItem.toLowerCase());
 
-            return indexA - indexB;
-        });
+                return indexA - indexB;
+            }
+        );
 };
 
 export default {
@@ -33,28 +49,34 @@ export default {
             },
             set(item: string) {
                 this.temporaryItem = item;
-                this.matchingItems = getMatchingItems(this.items, this.temporaryItem);
+                this.matchingItems = getMatchingItems(
+                    this.items,
+                    this.temporaryItem
+                );
                 this.listVisible = this.shouldDisplayList();
 
                 this.$emit("input", item);
-            }
-        }
+            },
+        },
     },
     created() {
-        this.debouncedOnBlur = debounce(this.onBlur, 500)
+        this.debouncedOnBlur = debounce(this.onBlur, 500);
     },
     data() {
         return {
             fieldActive: false,
             listVisible: false,
             matchingItems: [] as string[],
-            temporaryItem: ""
+            temporaryItem: "",
         };
     },
     inheritAttrs: false,
     methods: {
         getHtmlLabelForItem(item: string): string {
-            return item.replace(new RegExp(`(${ this.currentItem })`, "i"), "<mark>$1</mark>");
+            return item.replace(
+                new RegExp(`(${this.currentItem})`, "i"),
+                "<mark>$1</mark>"
+            );
         },
         isCurrentItem(item: string): boolean {
             return item.trim() === this.temporaryItem.trim();
@@ -77,7 +99,10 @@ export default {
         onListItemSelected(event: KeyboardEvent) {
             event.preventDefault();
 
-            if (this.listVisible && this.matchingItems.includes(this.temporaryItem)) {
+            if (
+                this.listVisible &&
+                this.matchingItems.includes(this.temporaryItem)
+            ) {
                 event.stopPropagation();
             }
 
@@ -92,38 +117,56 @@ export default {
             }
         },
         onListMoveDown() {
-            let nextItem: string = getNextItem(this.matchingItems, this.temporaryItem);
+            const nextItem: string = getNextItem(
+                this.matchingItems,
+                this.temporaryItem
+            );
+
             this.temporaryItem = !nextItem ? this.matchingItems[0] : nextItem;
             this.scrollToItem(this.temporaryItem);
         },
         onListMoveUp() {
-            let prevItem = getPreviousItem(this.matchingItems, this.temporaryItem);
-            this.temporaryItem = !prevItem ? this.matchingItems[this.matchingItems.length - 1] : prevItem;
+            const prevItem = getPreviousItem(
+                this.matchingItems,
+                this.temporaryItem
+            );
+
+            this.temporaryItem = !prevItem
+                ? this.matchingItems[this.matchingItems.length - 1]
+                : prevItem;
+
             this.scrollToItem(this.temporaryItem);
         },
         scrollToItem(item: string) {
-            let itemIndex = this.matchingItems.indexOf(item);
-            let listElement = this.$refs["value-list"];
-            let listElementRect = listElement.getBoundingClientRect();
-            let itemElement = listElement.querySelectorAll("li")[itemIndex];
-            let itemElementRect = itemElement.getBoundingClientRect();
+            const itemIndex = this.matchingItems.indexOf(item);
+            const listElement = this.$refs["value-list"];
 
-            let listBounds = {
+            if (!listElement) {
+                return;
+            }
+
+            const listElementRect = listElement.getBoundingClientRect();
+            const itemElement = listElement.querySelectorAll("li")[itemIndex];
+            const itemElementRect = itemElement.getBoundingClientRect();
+
+            const listBounds = {
                 top: listElement.scrollTop,
-                bottom: listElement.scrollTop + listElementRect.height
-            }
+                bottom: listElement.scrollTop + listElementRect.height,
+            };
 
-            let itemBounds = {
+            const itemBounds = {
                 top: itemElement.offsetTop,
-                bottom: itemElement.offsetTop + itemElementRect.height
-            }
+                bottom: itemElement.offsetTop + itemElementRect.height,
+            };
 
-            let outsideUpwards = itemBounds.top < listBounds.top;
-            let outsideDownwards = itemBounds.bottom > listBounds.bottom;
-            let outOfBounds = outsideUpwards || outsideDownwards;
+            const outsideUpwards = itemBounds.top < listBounds.top;
+            const outsideDownwards = itemBounds.bottom > listBounds.bottom;
+            const outOfBounds = outsideUpwards || outsideDownwards;
 
             if (outOfBounds) {
-                listElement.scrollTop = outsideUpwards ? itemBounds.top : itemBounds.bottom - listElementRect.height;
+                listElement.scrollTop = outsideUpwards
+                    ? itemBounds.top
+                    : itemBounds.bottom - listElementRect.height;
             }
         },
         shouldDisplayList(): boolean {
@@ -135,7 +178,10 @@ export default {
                 return false;
             }
 
-            if (this.matchingItems.length === 1 && this.matchingItems[0] === this.temporaryItem) {
+            if (
+                this.matchingItems.length === 1 &&
+                this.matchingItems[0] === this.temporaryItem
+            ) {
                 return false;
             }
 
@@ -143,15 +189,16 @@ export default {
         },
         updateCurrentValue(item: string) {
             this.currentItem = item;
-        }
+            this.onFocus();
+        },
     },
     props: {
         items: Array,
         value: String,
         keepFocus: {
             default: false,
-            type: Boolean
-        }
+            type: Boolean,
+        },
     },
     template: `
         <div class="auto-complete-field">
@@ -163,9 +210,10 @@ export default {
                 v-on="{ blur: debouncedOnBlur, focus: onFocus }"
                 :value="currentItem"
                 @input="updateCurrentValue($event.target.value)"
+                @keydown.down="onListMoveDown"
                 @keydown.enter="onListItemSelected"
-                @keyup.down="onListMoveDown"
-                @keyup.up="onListMoveUp">
+                @keydown.escape="debouncedOnBlur"
+                @keydown.up="onListMoveUp">
             <ul ref="value-list" class="auto-complete-list" v-if="listVisible">
                 <li
                     @click="onClick(item)"
@@ -175,5 +223,5 @@ export default {
                 </li>
             </ul>
         </div>
-    `
-}
+    `,
+};
